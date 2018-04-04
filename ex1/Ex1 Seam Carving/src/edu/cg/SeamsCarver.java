f30a88f1c2e3f8293826a7214d9d35720791a398
hillabarkal <hilla.guz@gmail.com>
2018-04-04 14:54:49 +0300

    I lost it. From scratch

--- ex1/Ex1 Seam Carving/src/edu/cg/SeamsCarver.java
+++ ex1/Ex1 Seam Carving/src/edu/cg/SeamsCarver.java
@@ -4,319 +4,66 @@ import java.awt.image.BufferedImage;
 import java.awt.Color;
 
 public class SeamsCarver extends ImageProcessor {
-	
-	//MARK: An inner interface for functional programming.
-	@FunctionalInterface
-	interface ResizeOperation {
-		BufferedImage apply();
-	}
-	
-	//MARK: Fields
-	private int numOfSeams;
-	private ResizeOperation resizeOp;
-	private int maxX;
-	
-	//TODO: Add some additional fields:
-	
-	
-	//MARK: Constructor
-	public SeamsCarver(Logger logger, BufferedImage workingImage,
-			int outWidth, RGBWeights rgbWeights) {
-		super(logger, workingImage, rgbWeights, outWidth, workingImage.getHeight()); 
-		
-		numOfSeams = Math.abs(outWidth - inWidth);
-		
-		if(inWidth < 2 | inHeight < 2)
-			throw new RuntimeException("Can not apply seam carving: workingImage is too small");
-		
-		if(numOfSeams > inWidth/2)
-			throw new RuntimeException("Can not apply seam carving: too many seams...");
-		
-		//Sets resizeOp with an appropriate method reference
-		if(outWidth > inWidth)
-			resizeOp = this::increaseImageWidth;
-		else if(outWidth < inWidth)
-			resizeOp = this::reduceImageWidth;
-		else
-			resizeOp = this::duplicateWorkingImage;
-		
-		//TODO: Initialize your additional fields and apply some preliminary calculations:
-		maxX = inWidth - 1;
-	}
-	
-	//MARK: Methods
-	public BufferedImage resize() {
-		return resizeOp.apply();
-	}
-	
-	//MARK: Unimplemented methods
-	private BufferedImage reduceImageWidth() {
-		return removeKseams();
-	}
-	
-	private BufferedImage increaseImageWidth() {
-		return addKseams();
-	}
-	
-	public BufferedImage showSeams(int seamColorRGB) {
-		return findKseams(seamColorRGB);
-	}
 
-	private BufferedImage findKseams(int seamColorRGB) {
-		BufferedImage outImg = duplicateWorkingImage();
-		BufferedImage greyImage = greyscale(outImg);
-		int[][] seams = new int[inHeight][inWidth];
-		for (int i = 0; i < numOfSeams; i++) {
-			// for reach seam calculate the costs matrix
-			int[][] pixelsEnergy = calcPixelsEnergy(greyImage);
-			long[][] cost = calcCostMatrix(greyImage, pixelsEnergy);
-			// optimal seam is minimum in lowest row
-			int y = inHeight - 1;
-			int minIndex = getMinIndex(cost[y]);
-			// backtrack cost matrix to get seam
-			int[] seam = getSeam(cost, minIndex, greyImage, pixelsEnergy);
-			// save seam, add all seams later
-			seams[i] = seam;
-			// remove the seam as to mark it done
-			outImg = removeSeamFrom(outImg, seam);
-			greyImage = greyscale(outImg);
-		}
+    //MARK: An inner interface for functional programming.
+    @FunctionalInterface
+    interface ResizeOperation {
+        BufferedImage apply();
+    }
 
-		// mark seams:
-		outImg = duplicateWorkingImage();
-		BufferedImage newOutImg = duplicateWorkingImage();
-		for (int i = 0; i < numOfSeams; i++) {
-			markSeamIn(newOutImg, seams[i], seamColorRGB);
-		}
+    //MARK: Fields
+    private int numOfSeams;
+    private ResizeOperation resizeOp;
+    private int maxX;
 
-		return outImg;
-	}
+    //TODO: Add some additional fields:
 
-	private BufferedImage addKseams() {
-		BufferedImage outImg = duplicateWorkingImage();
-		BufferedImage greyImage = greyscale(outImg);
-		int[][] seams = new int[inHeight][inWidth];
-		for (int i = 0; i < numOfSeams; i++) {
-			// for reach seam calculate the costs matrix
-			int[][] pixelsEnergy = calcPixelsEnergy(greyImage);
-			long[][] cost = calcCostMatrix(greyImage, pixelsEnergy);
-			// optimal seam is minimum in lowest row
-			int y = inHeight - 1;
-			int minIndex = getMinIndex(cost[y]);
-			// backtrack cost matrix to get seam
-			int[] seam = getSeam(cost, minIndex, greyImage, pixelsEnergy);
-			// save seam, add all seams later
-			seams[i] = seam;
-			// remove the seam as to mark it done
-			outImg = removeSeamFrom(outImg, seam);
-			greyImage = greyscale(outImg);
-		}
 
-		// add seams:
-		setForEachInputParameters();
-		outImg = duplicateWorkingImage();
-		for (int i = 0; i < numOfSeams; i++) {
-			int[] seam = seams[i];
-			outImg = addSeamTo(outImg, seam);
-		}
+    //MARK: Constructor
+    public SeamsCarver(Logger logger, BufferedImage workingImage,
+                       int outWidth, RGBWeights rgbWeights) {
+        super(logger, workingImage, rgbWeights, outWidth, workingImage.getHeight());
 
-		return outImg;
-	}
+        numOfSeams = Math.abs(outWidth - inWidth);
 
-	private BufferedImage removeKseams() {
-		BufferedImage outImg = duplicateWorkingImage();
-		BufferedImage greyImage = greyscale(outImg);
-		for (int i = 0; i < numOfSeams; i++) {
-			// for reach seam calculate the costs matrix
-			int[][] pixelsEnergy = calcPixelsEnergy(greyImage);
-			long[][] cost = calcCostMatrix(greyImage, pixelsEnergy);
-			// optimal seam is minimum in lowest row
-			int y = inHeight - 1;
-			int minIndex = getMinIndex(cost[y]);
-			// backtrack cost matrix to get seam
-			int[] seam = getSeam(cost, minIndex, greyImage, pixelsEnergy);
-			// remove the seam
-			outImg = removeSeamFrom(outImg, seam);
-			greyImage = greyscale(outImg);
-		}
-		return outImg;
-	}
+        if (inWidth < 2 | inHeight < 2)
+            throw new RuntimeException("Can not apply seam carving: workingImage is too small");
 
-	public BufferedImage removeSeamFrom(BufferedImage img, int[] seam) {
-		int height = img.getHeight();
-		int newWidth = img.getWidth() - 1;
-		maxX = newWidth - 1;
-		BufferedImage outImg = newEmptyImage(newWidth, height);
-		setForEachParameters(newWidth, height);
-		forEach((y, x) -> {
-			if (x < seam[y] && x != newWidth - 1) {
-				// current x is left to the seam - copy regularly
-				outImg.setRGB(x, y, img.getRGB(x, y));
-			} else if (x != newWidth - 1) {
-				// current x is right to the seam - shift
-				outImg.setRGB(x, y, img.getRGB(x + 1, y));
-			}
-		});
+        if (numOfSeams > inWidth / 2)
+            throw new RuntimeException("Can not apply seam carving: too many seams...");
 
-		return outImg;
-	}
+        //Sets resizeOp with an appropriate method reference
+        if (outWidth > inWidth)
+            resizeOp = this::increaseImageWidth;
+        else if (outWidth < inWidth)
+            resizeOp = this::reduceImageWidth;
+        else
+            resizeOp = this::duplicateWorkingImage;
 
-	public BufferedImage addSeamTo(BufferedImage img, int[] seam) {
-		int height = img.getHeight();
-		int newWidth = img.getWidth() + 1;
-		maxX = newWidth - 1;
-		BufferedImage outImg = newEmptyImage(newWidth, height);
-		setForEachParameters(newWidth, height);
-		forEach((y, x) -> {
-			if (x < seam[y]) {
-				// current x is left to the seam - copy regularly
-				outImg.setRGB(x, y, img.getRGB(x, y));
-			} else {
-				// current x is right to the seam - shift
-				outImg.setRGB(x, y, img.getRGB(x - 1, y));
-			}
-		});
-		setForEachInputParameters();
+        //TODO: Initialize your additional fields and apply some preliminary calculations:
+        maxX = inWidth - 1;
+    }
 
-		return outImg;
-	}
+    //MARK: Methods
+    public BufferedImage resize() {
+        return resizeOp.apply();
+    }
 
-	public void markSeamIn(BufferedImage img, int[] seam, int seamColorRGB) {
-		setForEachParameters(img.getWidth(), img.getHeight());
-		forEach((y, x) -> {
-			if (x != seam[y]) {
-				// current x is left to the seam - copy regularly
-				img.setRGB(x, y, img.getRGB(x, y));
-			} else {
-				// current x is right to the seam - shift
-				img.setRGB(x, y, seamColorRGB);
-			}
-		});
-	}
+    //MARK: Unimplemented methods
+    private BufferedImage reduceImageWidth() {
+        //TODO: Implement this method, remove the exception.
+        throw new UnimplementedMethodException("reduceImageWidth");
+    }
 
-	public int[] getSeam(long[][] cost, int minIndex, BufferedImage greyImage, int[][] pixelsEnergy) {
-		//  the entry x at index y in the array represent pixel (y, x) in the seam
-		int[] seam = new int[inHeight];
-		int j = minIndex;
-		seam[inHeight - 1] = j;
+    private BufferedImage increaseImageWidth() {
+        //TODO: Implement this method, remove the exception.
+        throw new UnimplementedMethodException("reduceImageWidth");
+    }
 
-		for (int i = inHeight - 2; i > 0; i--) {
-			int p = pixelsEnergy[i][j];
-			if (cost[i][j] == p + cost[i - 1][j] + calcCV(j, i, greyImage)) {
-				seam[i] = j;
-			} else if (j != 0 && cost[i][j] == p + cost[i - 1][j - 1] + calcCL(j, i, greyImage)) {
-				seam[i] = j - 1;
-			} else {
-				seam[i] = j + 1;
-			}
+    public BufferedImage showSeams(int seamColorRGB) {
+        //TODO: Implement this method, remove the exception.
+        throw new UnimplementedMethodException("reduceImageWidth");
+    }
 
-			j = seam[i];
-		}
 
-		return seam;
-	}
-
-	public int getMinIndex(long[] inArray) {
-		long minVal = inArray[0];
-		int minIndex = 0;
-		for (int i = 1; i < inArray.length; i++) {
-			if (inArray[i] < minVal) {
-				minVal = inArray[i];
-				minIndex = i;
-			}
-		}
-		return minIndex;
-	}
-
-	private long[][] calcCostMatrix(BufferedImage greyImage, int[][] pixelsEnergy) {
-		int width = greyImage.getWidth();
-		int height = greyImage.getHeight();
-		long[][] cost = new long[height][width];
-		setForEachParameters(width, height);
-		forEach((y, x) -> {
-			int p = pixelsEnergy[y][x];
-			if (y == 0) {
-				cost[y][x] = p;
-			} else if (x == 0) {
-				cost[y][x] = p +
-						Math.min((cost[y - 1][x] + calcCV(x, y, greyImage)),
-								(cost[y - 1][x + 1] + calcCR(x, y, greyImage)));
-			} else if (x == width - 1) {
-				cost[y][x] = p +
-						Math.min((cost[y - 1][x - 1] + calcCL(x, y, greyImage)),
-								(cost[y - 1][x] + calcCV(x, y, greyImage)));
-			} else {
-				int CL = calcCL(x, y, greyImage);
-				int CV = calcCV(x, y, greyImage);
-				int CR = calcCR(x, y, greyImage);
-				cost[y][x] = p +
-						Math.min((Math.min((cost[y - 1][x - 1] + CL),
-								(cost[y - 1][x] + CV))),
-								(cost[y - 1][x + 1] + CR));
-			}
-		});
-		setForEachInputParameters();
-
-		return cost;
-	}
-
-	private int calcPixelEnergy(int x, int y, BufferedImage greyImage) {
-		if (x < greyImage.getWidth() - 1) {
-			int p = new Color(greyImage.getRGB(x, y)).getBlue();
-			int p1 = new Color(greyImage.getRGB(x + 1, y)).getBlue();
-			return Math.abs(p - p1);
-		}
-
-		int p = new Color(greyImage.getRGB(x, y)).getBlue();
-		int p1 = new Color(greyImage.getRGB(x - 1, y)).getBlue();
-		return Math.abs(p - p1);
-	}
-
-	private int[][] calcPixelsEnergy(BufferedImage greyImage) {
-		int width = greyImage.getWidth();
-		int height = greyImage.getHeight();
-		int[][] pEnergy = new int[height][width];
-		setForEachParameters(width, height);
-		forEach((y, x) -> {
-			pEnergy[y][x] = calcPixelEnergy(x, y, greyImage);
-		});
-		setForEachInputParameters();
-		return pEnergy;
-	}
-
-	private int calcCL(int x, int y, BufferedImage greyImage) {
-		if (x == maxX) {
-			int p1 = new Color(greyImage.getRGB(x, y - 1)).getBlue();
-			int p2 = new Color(greyImage.getRGB(x - 1, y)).getBlue();
-			return Math.abs(p1 - p2);
-		}
-
-		int p1 = new Color(greyImage.getRGB(x, y - 1)).getBlue();
-		int p2 = new Color(greyImage.getRGB(x - 1, y)).getBlue();
-		int p3 = new Color(greyImage.getRGB(x + 1, y)).getBlue();
-		return Math.abs(p1 - p2) + Math.abs(p2 - p3);
-	}
-
-	private int calcCV(int x, int y, BufferedImage greyImage) {
-		if (x == 0 || x == maxX) {
-			return 0;
-		}
-
-		int p2 = new Color(greyImage.getRGB(x - 1, y)).getBlue();
-		int p3 = new Color(greyImage.getRGB(x + 1, y)).getBlue();
-		return Math.abs(p3 - p2);
-	}
-
-	private int calcCR(int x, int y, BufferedImage greyImage) {
-		if (x == 0) {
-			int p1 = new Color(greyImage.getRGB(x, y - 1)).getBlue();
-			int p3 = new Color(greyImage.getRGB(x + 1, y)).getBlue();
-			return Math.abs(p1 - p3);
-		}
-
-		int p1 = new Color(greyImage.getRGB(x, y - 1)).getBlue();
-		int p2 = new Color(greyImage.getRGB(x - 1, y)).getBlue();
-		int p3 = new Color(greyImage.getRGB(x + 1, y)).getBlue();
-		return Math.abs(p1 - p3) + Math.abs(p2 - p3);
-	}
 }
