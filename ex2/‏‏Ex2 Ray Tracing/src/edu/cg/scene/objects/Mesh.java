a63daf68adc355859c76372251b21ff5332befb6
hillabarkal <hilla.guz@gmail.com>
2018-05-04 11:44:21 +0300

    Mehadash

--- ex2/‏‏Ex2 Ray Tracing/src/edu/cg/scene/objects/Mesh.java
+++ ex2/‏‏Ex2 Ray Tracing/src/edu/cg/scene/objects/Mesh.java
@@ -12,43 +12,43 @@ import edu.cg.algebra.Ray;
 public class Mesh extends Shape implements Iterable<Triangle> {
 	public class Triplet {
 		private int i1, i2, i3;
-
+		
 		public Triplet() {
 			i1 = i2 = i3 = 0;
 		}
-
+		
 		public Triplet(int i1, int i2, int i3) {
 			this.i1 = i1;
 			this.i2 = i2;
 			this.i3 = i3;
 		}
-
+		
 		public Triangle makeTriangle() {
 			return new Triangle(vertices.get(i1), vertices.get(i2), vertices.get(i3));
 		}
-
+		
 		@Override
 		public String toString() {
 			return makeTriangle().toString();
 		}
 	}
-
+	
 	private List<Point> vertices;
 	private List<Triplet> indices;
-
+	
 	@Override
 	public String toString() {
 		String endl = System.lineSeparator();
-		return "Mesh:" + endl +
+		return "Mesh:" + endl + 
 				"Vertices:" + endl + vertices + endl +
 				"Triangles: " + indices + endl;
 	}
-
+	
 	@Override
 	public Iterator<Triangle> iterator() {
 		return new Iterator<Triangle>() {
 			private int currentTripletIndex = 0;
-
+			
 			@Override
 			public boolean hasNext() {
 				return currentTripletIndex < indices.size();
@@ -58,7 +58,7 @@ public class Mesh extends Shape implements Iterable<Triangle> {
 			public Triangle next() {
 				if(!hasNext())
 					throw new NoSuchElementException();
-
+				
 				return indices.get(currentTripletIndex++).makeTriangle();
 			}
 		};
